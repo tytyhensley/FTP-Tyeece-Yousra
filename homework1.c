3ec615e8575b66924b9ee987372f0fa8b30cecc1
tytyhensley <tkh265@nyu.edu>
2020-03-03 16:33:22 +0400

    ty

--- homework1.c
+++ homework1.c
@@ -1,1 +1,10 @@
-
+#include <stdio.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <arpa/inet.h>
